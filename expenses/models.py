8686d1bb97bf7a60c12d39b697699a10a205156e
Mert Cetin <mert.cetin20@gmail.com>
2025-01-23 14:47:37 +0300

    initial commit

---
+++ expenses/models.py
@@ -0,0 +1,41 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Kategori modeli
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.name
+    
+    # Category modelinin adının çoğul formunu belirler (Categorys olmasın diye)
+    class Meta:
+        verbose_name_plural = "Categories"
+
+
+# Giderleri temsil eden model
+class Expense(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    currency = models.CharField(max_length=10)
+    date = models.DateField()
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    
+    # Kaydın oluşturulma zamanını otomatik olarak kaydeden attribute
+    added_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.title} - {self.amount} USD"
+    
+
+# Bütçe modeli
+class Budget(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    month = models.DateField()
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    
+    def __str__(self):
+        return f"Budget for {self.month.strftime('%B %Y')}"
+    
